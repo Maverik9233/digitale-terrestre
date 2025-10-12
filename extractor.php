b08aaed0d7f2551547af63bcc4f05bb4f3f52c1f
Maverik9233 <maverikinter92@gmail.com>
2025-10-12 18:15:44 +0200

    Update extractor.php

--- extractor.php
+++ extractor.php
@@ -1,19 +1,36 @@
 <?php
-// Disattiva limiti e avvisi
 error_reporting(0);
 header('Content-Type: application/json');
 
-// Scarica la homepage
-$html = @file_get_contents("https://www.vrsicilia.it/");
-if(!$html) {
+// Lista dei siti da cui estrarre lo streaming
+$sites = [
+  "vrsicilia" => "https://www.vrsicilia.it/",
+  "canale2"   => "",
+  "canale3"   => ""
+];
+
+// Legge quale canale richiedere (es: ?site=vrsicilia)
+$siteKey = $_GET['site'] ?? "vrsicilia";
+
+if (!isset($sites[$siteKey])) {
+  echo json_encode(["error" => "Sito non trovato"]);
+  exit;
+}
+
+// Scarica la pagina del sito
+$html = @file_get_contents($sites[$siteKey]);
+if (!$html) {
   echo json_encode(["error" => "Impossibile contattare il sito"]);
   exit;
 }
 
-// Trova l’URL .m3u8 con una regex
-if(preg_match('/https[^\'"]+\.m3u8[^\'"]*/', $html, $match)) {
-  echo json_encode(["url" => $match[0]]);
+// Cerca il link .m3u8
+if (preg_match('/https[^\'"]+\.m3u8[^\'"]*/', $html, $match)) {
+  echo json_encode([
+    "site" => $siteKey,
+    "url"  => $match[0]
+  ]);
 } else {
-  echo json_encode(["error" => "Nessun link trovato"]);
+  echo json_encode(["error" => "Nessun link trovato per $siteKey"]);
 }
 ?>
